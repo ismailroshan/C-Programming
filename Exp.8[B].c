ba575dc8884ba779c8e8137cd9cd9ad369a1def0
Ismail roshan <ismailroshan504@eng.rizvi.edu.in>
2025-11-20 17:30:07 +0530

    Create Exp.8[B].c

---
+++ Exp.8[B].c
@@ -0,0 +1,31 @@
+/*
+Name: ISMAIL YUSUF ROSHAN 
+UIN: 251M035
+Class: FE Mechanical Engineering
+Div: F
+*/
+#include <stdio.h>
+int main() {
+    char str[100];
+    int i, j, flag = 0;
+
+    printf("Enter a string: ");
+    scanf("%s", str);
+
+    for (i = 0; str[i] != '\0'; i++);
+
+  
+    for (j = 0; j < i / 2; j++) {
+        if (str[j] != str[i - j - 1]) {
+            flag = 1;
+            break;
+        }
+    }
+
+    if (flag == 0)
+        printf("The string is a palindrome.");
+    else
+        printf("The string is NOT a palindrome.");
+
+    return 0;
+}
